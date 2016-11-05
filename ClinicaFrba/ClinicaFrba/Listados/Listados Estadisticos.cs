f8b8beebc63b06bed4c1626f81c453fa5cef9617
Mauro Corvaro <corvaro.mauro@gmail.com>
2016-11-05 00:48:57 -0300

    Form listados estadisticos

--- ClinicaFrba/ClinicaFrba/Listados/Listados Estadisticos.cs
+++ ClinicaFrba/ClinicaFrba/Listados/Listados Estadisticos.cs
@@ -16,5 +16,15 @@ namespace ClinicaFrba.Listados
         {
             InitializeComponent();
         }
+
+        private void lbIngreseNroDoc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
