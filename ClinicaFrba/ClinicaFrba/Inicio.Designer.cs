bf361337cd5e7db57ec273a87a60fc06e0ea7829
Mica21 <mika_2195@hotmail.com>
2016-11-07 09:44:34 -0300

    Limpia soluciones+ archivoconf

--- ClinicaFrba/ClinicaFrba/Inicio.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Inicio.Designer.cs
@@ -117,7 +117,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(804, 366);
+            this.ClientSize = new System.Drawing.Size(804, 361);
             this.Controls.Add(this.lbContraseñaIncorrecta);
             this.Controls.Add(this.btIniciarSesión);
             this.Controls.Add(this.txtContraseña);
